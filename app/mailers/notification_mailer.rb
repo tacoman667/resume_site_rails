95f4bef4dcf9f3dc3da8677db2310bbcd24350b0
Jeremy The SuperGeek <jeremy@codingzen.net>
2014-08-25 10:46:24 -0500

    added default from address

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,9 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "noreply@#{ENV['DEFAULT_MAIL_HOST']}"
 
   def pdf_viewed(ip_address)
   	@scraper = IpLookupScraper.new(ip_address)
   	admin = Admin.first
-  	p "noreply@#{ActionMailer::Base.default_url_options[:host]}"
 		mail(to: admin.email, subject: 'Someone looked at your resume').deliver
   end
 end
