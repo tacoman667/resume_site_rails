724f8c11c60ab808c05aef3d9cb2493712db93fb
Jeremy The SuperGeek <jeremy@codingzen.net>
2014-08-25 10:11:08 -0500

    changed default from to be a no-reply address using the current host.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@#{ActionMailer::Base.default_url_options[:host]}"
 
   def pdf_viewed(ip_address)
   	@scraper = IpLookupScraper.new(ip_address)
