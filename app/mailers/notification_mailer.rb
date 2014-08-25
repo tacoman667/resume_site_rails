22d1adfa90acea954c1d7ba0e10da5d6b8b45fca
Jeremy The SuperGeek <jeremy@codingzen.net>
2014-08-25 10:14:07 -0500

    removed hyphen in email address.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "no-reply@#{ActionMailer::Base.default_url_options[:host]}"
+  default from: "noreply@#{ActionMailer::Base.default_url_options[:host]}"
 
   def pdf_viewed(ip_address)
   	@scraper = IpLookupScraper.new(ip_address)
